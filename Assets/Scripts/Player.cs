0af9287abf40f5e13d9f8483de3ae2e48469b801
DESKTOP-MS50HD7\user <marshalonah@gmail.com>
2024-03-02 08:02:36 +0100

    basic movement and rotation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+
+        Vector2 inputVec = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.W)) inputVec.y = +1f;
+
+        if (Input.GetKey(KeyCode.A)) inputVec.x = -1f;
+
+        if(Input.GetKey(KeyCode.S)) inputVec.y = -1f;
+
+        if (Input.GetKey(KeyCode.D)) inputVec.x = +1f;
+
+        inputVec = inputVec.normalized;
+
+        Vector3 moveDir = new Vector3(inputVec.x, 0f, inputVec.y);
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        //Face the movement direction
+        float rotSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotSpeed);
+    }
+}
